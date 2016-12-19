6afd787c34a57ed89b57bb1ef52ffdbcb616e61f
Alexey Kremsa <alexey.kremsa@cactussoft.biz>
2016-12-19 17:50:07 +0300

    simple chat room

--- client.go
+++ client.go
@@ -13,5 +13,25 @@ type client struct {
 
 func (c *client) read() {
 	defer c.socket.Close()
+	for {
+		_, msg, err := c.socket.ReadMessage()
 
+		if err != nil {
+			return
+		}
+
+		c.room.forward <- msg
+	}
+}
+
+func (c *client) write() {
+	defer c.socket.Close()
+
+	for msg := range c.send {
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+
+		if err != nil {
+			return
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,26 +1,37 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-        <html>
-            <head>
-                <title>Chat</title>
-            </head>
-            <body>
-            Lets chat !
-            </body>
-        </html>`))
-	})
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	go r.run()
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("LiestenAndServe: ", err)
 	}
-
 }
